81c6bf4b16e6a8d0d170ef9fea6cabc18496d932
kolik29 <30481034+kolik29@users.noreply.github.com>
2017-08-18 06:26:50 +0300

    Add files via upload

---
+++ TA/js/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+	var menu_open = false;
+	$('#switch_list').click(function() {
+		if (menu_open) {
+			$('input[name="search"]').css('margin-left', '10px');
+			$('.device_list').css('margin-left', '0px');
+			$('.work_display').css('width', 'calc(100% - 234px)');
+		}
+		else {
+			$('input[name="search"]').css('margin-left', '-250px');
+			$('.device_list').css('margin-left', '-234px');
+			$('.work_display').css('width', '100%');
+			if ($('.work_display').scrollLeft() != 0) {
+				$('.work_display').animate({
+					scrollLeft:  $('.main_work').width(),
+				}, 300);
+			}
+		}
+		menu_open = !menu_open;
+	});
+
+	$('.device_block').click(function() {
+		$('.main_work').css('width', '200%');
+		$('.workpanel').css('width', 'calc(50% - 40px)');
+		$('#current_sensor').html("<span>&nbsp>&nbsp</span><span class='path_control'>" + this.textContent + "</span>");
+		$('.work_display').animate({
+			scrollLeft:  $('.main_work').width(),
+		}, 500);
+	});
+
+	$('#current_sensor').click(function() {
+		$('.work_display').animate({
+			scrollLeft:  $('.main_work').width(),
+		}, 500);
+	});
+
+	$('#gotoFacade').click(function() {
+		$('.work_display').animate({
+			scrollLeft:  0,
+		}, 500);
+	});
+	
+	get_markup();
+
+	function get_markup() {
+      	var canvas = document.getElementById('current_voltage_graph');
+      	var context = canvas.getContext('2d');
+
+    	context.beginPath();
+    	context.lineWidth = 2;
+    	context.strokeStyle = 'black';
+    	context.moveTo(10, 0);
+    	context.lineTo(10, 400);
+    	context.moveTo(0, 390);
+    	context.lineTo(722, 390);
+    	context.stroke();
+  	}
+});
