d936d7492032eaa25e98186c2e6fe064fa677885
Nikolay <kolik29@gmail.com>
2017-12-31 13:17:45 +0300

    update nre code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-	$('#moreWorks').click(function() {
-		
+$(document).ready(function() {
+	$(window).resize(function() {
+		resizer();
 	});
+	resizer();
+
+	function resizer() {
+		$('.v-sidebar__background').css('margin-left', '-' + (($('.v-sidebar__background').width() - $('.o-sidebar').width()) / 2) + 'px'); //выравнивает картинку на сайдбаре
+		$('.size-4-4').css('min-width', ($('.o-content').width() / 2) + 'px');
+	}
+
 });
