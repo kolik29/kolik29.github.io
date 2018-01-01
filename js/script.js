ae1ad00a6f83015ee7b36037d8b0bc5d25f6fb7d
Nikolay <kolik29@gmail.com>
2018-01-01 20:09:00 +0300

    1.1.18

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 		$(this).addClass('enable');
 	});
 
-	$('#js-myworks-item').click(function() {
+	$('#js-portfolio-item').click(function() {
 		$('.o-slide').animate({
 			top: -$(window).height()
 		}, 500);
@@ -38,44 +38,9 @@ $(document).ready(function() {
     	onWheel();
     });
 
-    var lastCall = 0;
-	var tempScrollTop, currentScrollTop = 0;
-    var n = 0;
-    function onWheel(e) {    	
-    	var now = Date.now();
-  		if(now - lastCall > 500) {
-      		e = e || window.event;      
-	  		var delta = e.deltaY || e.detail || e.wheelDelta;
-     		n += delta;
-     		if (currentScrollTop < n) { //down
-				$('.v-menu__item').each(function() {
-					if ($(this).hasClass('enable')) {
-						if ($(this).next().attr('id') != undefined) {
-							disableMenuItem();
-							$(this).next().addClass('enable');
-							$(this).next().trigger('click');
-							return false;
-						}
-					}
-				});
-			}
-      		else 
-      			if (currentScrollTop > n) { //up
-					$('.v-menu__item').each(function() {
-						if ($(this).hasClass('enable')) {
-							if ($(this).prev().attr('id') != undefined) {
-								disableMenuItem();
-								$(this).prev().addClass('enable');
-								$(this).prev().trigger('click');
-								return false;
-							}
-						}
-					});
-      			}
-      		currentScrollTop = n;
-     		lastCall = now;
-      	}
-    }
+    $('.v-btn.scroll-down').click(function() {
+    	$('#js-portfolio-item').trigger('click');
+    });
 });
 
 
@@ -84,4 +49,44 @@ function disableMenuItem() {
 	$('.v-menu__item').each(function() {
 		$(this).removeClass('enable');
 	});
+}
+
+
+var lastCall = 0;
+var tempScrollTop, currentScrollTop = 0;
+var n = 0;
+function onWheel(e) {    	
+    var now = Date.now();
+  	if(now - lastCall > 500) {
+      	e = e || window.event;      
+	  	var delta = e.deltaY || e.detail || e.wheelDelta;
+     	n += delta;
+    	if (currentScrollTop < n) { //down
+			$('.v-menu__item').each(function() {
+				if ($(this).hasClass('enable')) {
+					if ($(this).next().attr('id') != undefined) {
+						disableMenuItem();
+						$(this).next().addClass('enable');
+						$(this).next().trigger('click');
+						return false;
+					}
+				}
+			});
+		}
+    	else 
+     		if (currentScrollTop > n) { //up
+				$('.v-menu__item').each(function() {
+					if ($(this).hasClass('enable')) {
+						if ($(this).prev().attr('id') != undefined) {
+							disableMenuItem();
+							$(this).prev().addClass('enable');
+							$(this).prev().trigger('click');
+							return false;
+						}
+					}
+				});
+  			}
+   		currentScrollTop = n;
+  		lastCall = now;
+  	}
 }
