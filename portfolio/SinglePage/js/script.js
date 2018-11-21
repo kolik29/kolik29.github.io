19f7dde63c786cd15d65a78339893d2d2f742a3f
Nikolay <kolik29@gmail.com>
2018-11-22 01:14:09 +0300

    SinglePage готов

--- portfolio/SinglePage/js/script.js
+++ portfolio/SinglePage/js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-
+	
 });
+
+ymaps.ready(init);
+function init(){ 
+    var myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 7
+    });
+    myMap.behaviors.disable('scrollZoom');
+}
