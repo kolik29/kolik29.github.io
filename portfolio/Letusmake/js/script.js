82c9a3222d4bb8c5a07742d30982d421b1666411
Nikolay <kolik29@gmail.com>
2018-12-15 14:06:11 +0300

    1

--- portfolio/Letusmake/js/script.js
+++ portfolio/Letusmake/js/script.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
-	
+	$('a[href*="#"]').on('click', function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 1000);
+		e.preventDefault();
+		return false;
+	});
 });
 
 ymaps.ready(init);
-function init(){ 
+function init(){
     var myMap = new ymaps.Map("map", {
         center: [55.736530, 37.487361],
         zoom: 15,
