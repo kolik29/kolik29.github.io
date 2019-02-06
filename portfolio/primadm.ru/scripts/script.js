7b385ea3d7033474af421cb5e41de49c74584372
kolik29 <30481034+kolik29@users.noreply.github.com>
2019-02-06 17:15:03 +0300

    upd primadm.ru

--- portfolio/primadm.ru/scripts/script.js
+++ portfolio/primadm.ru/scripts/script.js
@@ -1,2 +1,102 @@
 $(document).ready(function() {
+	loadSlider($('#news_slider'), true);
+	loadSlider($('#photo_slider'), false, 3);
 });
+
+function loadSlider(slider, addDotted = false, count = 1) {
+	slider.find('#button-next').on('click', function () { slide('next'); });
+	slider.find('#button-prev').on('click', function () { slide('prev'); });
+
+	var width = (100 / count) + '%';
+
+	slider.find('.line').children().each(function() {
+		$(this).width(width);
+	});
+
+	if (addDotted) {
+		slider.append('<div id="dotted">');
+
+		slider.find('.line').children().each(function() {
+			slider.find('#dotted').append('<span>');
+		});
+		slider.find('#dotted').children().eq(0).addClass('select');
+	}
+
+	function slide(side) {
+		slider.find('#button-next').off('click');
+		slider.find('#button-prev').off('click');
+
+		switch (side) {
+			case 'next':
+				slider.find('.line > *').first().css({
+					'margin-left': '-' + width
+				});
+				setTimeout(() => {
+					slider.find('.line').append(
+						slider.find('.line > *').first().css({
+							'margin-left': '0'
+						})
+					);
+				}, 300);
+				break;
+
+			case 'prev':
+				slider.find('.line').prepend(
+					slider.find('.line > *').last().css({
+						'margin-left': '-' + width
+					})
+				);
+				setTimeout(() => {
+					slider.find('.line > *').first().css({
+						'margin-left': '0'
+					})
+				}, 1);
+				break;
+
+			default:
+				break;
+		}
+
+		dott(side);
+
+		setTimeout(() => {
+			slider.find('#button-next').on('click', function () { slide('next'); });
+			slider.find('#button-prev').on('click', function () { slide('prev'); });
+		}, 300);
+	}
+
+	function dott(side) {
+		var currentDot = 0;
+		if (addDotted) {
+			$('#dotted > *').each(function() {
+				if ($(this).hasClass('select'))
+					currentDot = $(this).index();
+
+				$(this).removeClass('select');
+			});
+		}
+
+		switch (side) {
+			case 'next':
+				if (addDotted) {
+					if ($('#dotted > *').length == currentDot + 1)
+						$('#dotted > *').eq(0).addClass('select');
+					else
+						$('#dotted > *').eq(currentDot + 1).addClass('select');
+				}
+				break;
+
+			case 'prev':
+				if (addDotted) {
+					if ($('#dotted > *').length == 0)
+						$('#dotted > *').eq($('#dotted > *').length - 1).addClass('select');
+					else
+						$('#dotted > *').eq(currentDot - 1).addClass('select');
+				}
+				break;
+
+			default:
+				break;
+		}
+	}
+}
