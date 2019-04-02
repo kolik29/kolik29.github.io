56622bc025859dbdebcc33d731c80219809a941c
kolik29 <30481034+kolik29@users.noreply.github.com>
2019-04-02 06:31:25 +0300

    Добавил ajax
    Для отправки письма

--- portfolio/kulikoff.online/scripts/script.js
+++ portfolio/kulikoff.online/scripts/script.js
@@ -26,4 +26,47 @@ function addEvents() {
 	$('#slider > .controls *').on('click', function() {
 		slide($(this));
 	});
+
+	$('#feedback #submit').on('click', function() {
+		var currentObj = $(this).parent();
+
+		if (currentObj.children('#name').val() == '' ||
+			currentObj.children('#email').val() == '' ||
+			currentObj.children('#text').val() == '') {
+			alert('Пожалуйста, заполните все поля!');
+
+			if (currentObj.children('#name').val() == '')
+				currentObj.children('#name').addClass('error');
+			else
+				currentObj.children('#name').removeClass('error');
+
+			if (currentObj.children('#email').val() == '')
+				currentObj.children('#email').addClass('error');
+			else
+				currentObj.children('#email').removeClass('error');
+
+			if (currentObj.children('#text').val() == '')
+				currentObj.children('#text').addClass('error');
+			else
+				currentObj.children('#text').removeClass('error');
+		} else if (!validateEmail(currentObj.children('#email').val())) {
+			alert('Некорректно введен email!');
+			currentObj.children('#email').addClass('error');
+		} else {
+			currentObj.children('#email').removeClass('error');
+
+			$.ajax({
+				type: "POST",
+				url: "https://getsimpleform.com/messages?form_api_token=f201ba4de9e54f56a13391c8881f55db",
+				data: {
+					form_api_token: "f201ba4de9e54f56a13391c8881f55db",
+					name: currentObj.children('#name').val(),
+					email: currentObj.children('#email').val(),
+					text: currentObj.children('#text').val()
+				}
+			}).done(function(msg) {
+				alert(msg);
+			});
+		}
+	});
 }
