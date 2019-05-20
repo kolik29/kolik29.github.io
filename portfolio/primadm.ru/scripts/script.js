934d44b91920ff7cdec702afeb2f48473b48562e
Куликов Николай Алексеевич <Assist@PR.local>
2019-05-20 13:19:28 +0300

    upd

--- portfolio/primadm.ru/scripts/script.js
+++ portfolio/primadm.ru/scripts/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-	
+	var h1 = $('.links-body a:nth-child(1)')[0].clientHeight;
+	var h2 = $('.links-body a:nth-child(5)')[0].clientHeight;
+
+	console.log(h1, h2)
+
+	$('.links-body').height(h1 + h2 + 5);
 });
