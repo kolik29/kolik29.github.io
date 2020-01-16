7598474048194967b69b9c68c5c92aaf085a214d
Nikolay <kolik29@gmail.com>
2020-01-16 17:30:21 +0300

    Добавил переключение навигации при смене слайдов

--- portfolio/kulikoff.online/scripts/script.js
+++ portfolio/kulikoff.online/scripts/script.js
@@ -11,35 +11,40 @@ $(document).ready(function() {
 		num = ((String(i + 1).length == 1) ? '0' + String(i + 1) : String(i + 1));
 
 		$('nav .left-side').append('<div class="item">' + num + '</div>');
-		$('nav .right-side').append('<div class="item">' + $(this).data('name') + '</div>');
+		$('nav .right-side').append('<div class="item"><div class="num">' + num + '</div><div class="name">' + $(this).data('name') + '</div></div>');
 	});
+
+	$('nav .left-side .item:first-child, nav .right-side .item:first-child').addClass('active');
 });
 
 var ignoreScroll = false;
 function scrollElement(e) {
 	if (e.deltaY != undefined) {
 		if (ignoreScroll == false) {
-			var active = $('section.active'), element = {};
+			var activeSection = $('section.active'), element = {};
 
 			if (e.deltaY < 0) {
 				element['show'] = true;
 
-				if (active.next().length != 0)
-					element['obj'] = active.next('section');
+				if (activeSection.next().length != 0)
+					element['obj'] = activeSection.next('section');
 				else
-					element['obj'] = active.parent().children().first();
+					element['obj'] = activeSection.parent().children().first();
 			}
 
 			if (e.deltaY > 0) {
 				element['show'] = false;
 
-				if (active.prev().length != 0)
-					element['obj'] = active.prev('section');
-				else
-					element['obj'] = active.parent().children().last();
+				if (activeSection.prev().length != 0)
+					element['obj'] = activeSection.prev('section');else
+					element['obj'] = activeSection.parent().children().last();
 			}
 
-			active.addClass('slide-' + (element['show'] ? 'hide' : 'show') + '-down').removeClass('active');
+			$('nav .left-side .item').removeClass('active');
+			$('nav .right-side .item').removeClass('active');
+			$('nav .left-side .item:eq(' + element['obj'].index() + '), nav .right-side .item:eq(' + element['obj'].index() + ')').addClass('active');
+
+			activeSection.addClass('slide-' + (element['show'] ? 'hide' : 'show') + '-down').removeClass('active');
 			element['obj'].addClass('active').addClass('slide-' + (element['show'] ? 'show' : 'hide') + '-up');
 
 			ignoreScroll = true;
