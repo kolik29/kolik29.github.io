31b54086f5f9dd3f79c39447fb6af229a71613dc
kolik29 <30481034+kolik29@users.noreply.github.com>
2020-06-16 23:02:28 +0300

    upd pub

--- portfolio/pub/script/script.js
+++ portfolio/pub/script/script.js
@@ -1,3 +1,10 @@
 $(() => {
-	
+	$('#btn-burger').click(() => {
+		if (!$('#mobilemenu').hasClass('show-left'))
+			$('#mobilemenu').addClass('show-left')
+	});
+	$('#mobilemenu-close').click(() => {
+		if ($('#mobilemenu').hasClass('show-left'))
+			$('#mobilemenu').removeClass('show-left')
+	});
 });
